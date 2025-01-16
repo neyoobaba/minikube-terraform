61d706e43afe09aa0e0630ec9129de38e7e83fbd
Neyoo <neyoobaba@yahoo.com>
2025-01-16 03:54:00 -0800

    chore: Initial commit

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_current_time():
+    current_time = time.strftime("%Y-%m%d %H:%M:%S", time.localtime())
+    return f"The current time: {current_time}"
+
+
+@app.route('/hello', methods=['GET'])
+def hellowrld():
+    if(request.method == 'GET'):
+        data = {"data": "Hello World Application"}
+        return jsonify(data)
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9001)
---
+++ terraform-configs/provider.tf
@@ -0,0 +1,7 @@
+provider "kubernetes" {
+  host = minikube_cluster.docker.host
+
+  client_certificate     = minikube_cluster.docker.client_certificate
+  client_key             = minikube_cluster.docker.client_key
+  cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
+}
