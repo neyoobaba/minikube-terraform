9c45417f2b8e58ee351ebd172834e6c036f56b7b
Neyoo <neyoobaba@yahoo.com>
2025-01-16 05:42:01 -0800

    chore: Add argocd and helm

--- terraform-configs/provider.tf
+++ terraform-configs/provider.tf
@@ -5,3 +5,9 @@ provider "kubernetes" {
   client_key             = minikube_cluster.docker.client_key
   cluster_ca_certificate = minikube_cluster.docker.cluster_ca_certificate
 }
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
